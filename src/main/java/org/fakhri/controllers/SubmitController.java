764cd68c8dedd0fe9d459fb86d645e34d5fbf039
Mufaddal Diwan <mufaddal.d.786@gmail.com>
2023-02-08 00:22:33 +0530

    calculator added

--- src/main/java/org/fakhri/controllers/SubmitController.java
+++ src/main/java/org/fakhri/controllers/SubmitController.java
@@ -2,6 +2,7 @@ package org.fakhri.controllers;
 
 import org.fakhri.entity.Gasket;
 import org.fakhri.entity.Material;
+import org.fakhri.service.Calculator;
 import org.fakhri.views.forms.GasketDetailsView;
 import org.fakhri.views.forms.MaterialDetailsView;
 
@@ -32,7 +33,7 @@ public class SubmitController implements Controller{
             Gasket gasket = gasketDetailsView.getSelectedItem();
             System.out.println(material);
             System.out.println(gasket);
-            outputLabel.setText("Calculated");
+            outputLabel.setText(String.valueOf(Calculator.calculate(material, gasket)));
 
         });
     }
---
+++ src/main/java/org/fakhri/service/Calculator.java
@@ -0,0 +1,11 @@
+package org.fakhri.service;
+
+import org.fakhri.entity.Gasket;
+import org.fakhri.entity.Material;
+
+public class Calculator {
+    public static int calculate(Material material, Gasket gasket) {
+//        TODO: dimensions in material and gasket are in same unit
+        return 100;
+    }
+}
